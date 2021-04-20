5088ef2b42f632263c7802bc6ccfab22810ab35a
Jada White <jadaw.2021@gmail.com>
2021-04-20 12:05:57 -0400

    Initial Commit

---
+++ hackathonCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hackathonCalculator
+//
+//  Created by Jada White on 4/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
