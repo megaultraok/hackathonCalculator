9dd18f81443609fb523413f5551195f8e5fd1b0c
Jada White <jadaw.2021@gmail.com>
2021-04-20 21:22:54 -0400

    Basic calculator functions (without percentage).

--- hackathonCalculator/ViewController.swift
+++ hackathonCalculator/ViewController.swift
@@ -8,10 +8,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var firstNum = 0.0
+    var secondNum = 0.0
+    var result = 0.0
+    var operation = ""
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".":
+            return outputLabel.text = outputLabel.text!.appending(sender.currentTitle!)
+        default:
+            print("Cannot access number button.")
+        }
+    }
+    
+    
+    @IBAction func dataButtonPressed(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "AC":
+            outputLabel.text!.removeAll()
+        case "+/-":
+            if outputLabel.text!.hasPrefix("-") {
+                outputLabel.text!.remove(at: outputLabel.text!.startIndex)
+            }
+            else {
+                outputLabel.text!.insert("-", at: outputLabel.text!.startIndex)
+            }
+        default:
+            print("Cannot access data button.")
+        }
+    }
 
+    @IBAction func operatorButtonPressed(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "÷":
+            operation = "division"
+            firstNum = Double(outputLabel.text!)!
+            outputLabel.text!.removeAll()
+        case "×":
+            operation = "multiplication"
+            firstNum = Double(outputLabel.text!)!
+            outputLabel.text!.removeAll()
+        case "−":
+            operation = "subtraction"
+            firstNum = Double(outputLabel.text!)!
+            outputLabel.text!.removeAll()
+        case "+":
+            operation = "addition"
+            firstNum = Double(outputLabel.text!)!
+            outputLabel.text!.removeAll()
+        default:
+            print("Cannot access operator button.")
+        }
+    }
+
+    @IBAction func equalButtonPressed(_ sender: UIButton) {
+        secondNum = Double(outputLabel.text!)!
+        
+        switch operation {
+        case "division":
+            result = firstNum / secondNum
+            outputLabel.text = String(result)
+        case "multiplication":
+            result = firstNum * secondNum
+            outputLabel.text = String(result)
+        case "subtraction":
+            result = firstNum - secondNum
+            outputLabel.text = String(result)
+        case "addition":
+            result = firstNum + secondNum
+            outputLabel.text = String(result)
+        default:
+            print("Cannot perform operation.")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        outputLabel.text = ""
     }
 
 
